0edeafa863cd7745091acecce22a66debf0fc564
Tony <arlidor@pronotmail.com>
2018-01-26 20:22:56 +0200

    add les 4

---
+++ WebLessons/Timur Batyrshinov - JavaBegin.ru/BasicsofJava/Lessons4/src/main/java/ru/habrahabr/arlidor/lessons4/Start.java
@@ -0,0 +1,14 @@
+package ru.habrahabr.arlidor.lessons4;
+
+public class Start {
+
+    public static void main(String args[]) {
+
+        String hi = "Hello";
+        int i = 4;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(i--);
+        System.out.println(i);
+    }
+}
